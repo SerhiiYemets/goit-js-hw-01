94258f9812280e195e68bc4699c2eae7b0a7ffef
Serhii Yemets <Serhiiyemets2020@gmail.com>
2025-08-02 13:54:20 +0200

    add task js

--- js/tack-3.js
+++ js/tack-3.js
@@ -2,7 +2,6 @@ function getElementWidth(content, padding, border) {
     const contentValue = parseFloat(content);
     const paddingValue = parseFloat(padding);
     const borderValue = parseFloat(border);
-
     const totalWidth = contentValue + 2 * paddingValue + 2 * borderValue;
     return totalWidth;
 }
